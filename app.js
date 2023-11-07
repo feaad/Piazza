e0d04dc843b478bcfd3dea549c074a45ab3025f5
feaad <antwi-donkor_f@outlook.com>
2023-11-07 14:46:43 +0000

    DB connection up and running...

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // importing libraries
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
+
+
 const app = express()
+app.use(bodyParser.json())
 
 
-
+mongoose.connect(process.env.DB_CONNECTOR).then(() => {
+    console.log('DB is running...')
+})
 
 app.listen(3000, () => {
     console.log('Server is running...')
